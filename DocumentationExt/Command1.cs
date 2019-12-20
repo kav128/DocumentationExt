c7de7a987d2cc380b769ad6b876d3b12dd72dda0
Артем Крылов <kav128@outlook.com>
2019-12-21 00:47:15 +0300

    Добавлен обработчик нажатия на пункт в меню

--- DocumentationExt/Command1.cs
+++ DocumentationExt/Command1.cs
@@ -90,7 +90,16 @@ namespace VSIXProject1
         /// <param name="e">Event args.</param>
         private void Execute(object sender, EventArgs e)
         {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            GenerateProject(
+                "C:\\Users\\kav128\\Documents\\Учеба\\5 семестр\\Инструментальные средства\\Lab1\\Lab\\bin\\Debug\\netcoreapp3.0\\InstrLab.xml",
+                "C:\\Users\\kav128\\Documents\\Учеба\\5 семестр\\Инструментальные средства\\Lab1\\Lab\\bin\\Debug\\netcoreapp3.0\\InstrLab.dll");
+
+            Thread thread = new Thread(CompileDocumentation);
+            thread.Start();
         }
+
         private static string GenerateProject(string xmlSource, string binSource)
         {
             XNamespace xmlns = "http://schemas.microsoft.com/developer/msbuild/2003";
